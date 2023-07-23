4536c8a7092d467b5ca912ee4ceb4fbec9c50d8a
Oliver Lee <oliverzlee@gmail.com>
2023-07-22 23:49:52 -0700

    Add a linear algebra library dependency (#9)
    Add `mdspan` and `stdBLAS` as dependencies in order to allow use of a
    linear algebra library.
    
    Change-Id: I9d2e1f43f6df315a922753d1d88e5904888b3d16

--- test/dummy_test.cpp
+++ test/dummy_test.cpp
@@ -1,15 +1,90 @@
 #include <boost/ut.hpp>
 
+#include <array>
+#include <cassert>
+#include <cstddef>
+#include <experimental/linalg>
+#include <experimental/mdspan>
 #include <fmt/core.h>
+
+namespace stx = std::experimental;
+
+template <class T, std::size_t R, std::size_t C>
+struct matrix
+{
+  static_assert(R != std::dynamic_extent);
+  static_assert(C != std::dynamic_extent);
+
+  using extents_type = stx::extents<std::size_t, R, C>;
+  using mdspan_type = stx::mdspan<T, extents_type>;
+  using const_mdspan_type = stx::mdspan<const T, extents_type>;
+
+  std::array<T, R * C> data{};
+
+  matrix() = default;
+
+  matrix(std::initializer_list<std::initializer_list<T>> init)
+  {
+    assert(R == init.size());
+
+    auto i = std::size_t{};
+    for (const auto& row : init) {
+      assert(C == row.size());
+
+      auto j = std::size_t{};
+      for (const auto& elem : row) {
+        span()[i, j] = elem;
+
+        ++j;
+      }
+
+      ++i;
+    }
+  }
+
+  [[nodiscard]]
+  constexpr auto span() -> mdspan_type
+  {
+    return mdspan_type{data.data()};
+  }
+  [[nodiscard]]
+  constexpr auto span() const -> const_mdspan_type
+  {
+    return const_mdspan_type{data.data()};
+  }
+
+  [[nodiscard]]
+  constexpr auto
+  operator[](std::size_t i, std::size_t j) -> mdspan_type::reference
+  {
+    return span()[i, j];
+  }
+  [[nodiscard]]
+  constexpr auto
+  operator[](std::size_t i, std::size_t j) const -> const_mdspan_type::reference
+  {
+    return span()[i, j];
+  }
+};
 
 auto main() -> int
 {
   using ::boost::ut::expect;
   using ::boost::ut::test;
 
+  // NOLINTBEGIN(readability-magic-numbers)
   test("true is true") = [] {
-    const auto s = fmt::format("The answer is {}.", 42);
+    using Mat = matrix<int, 2, 2>;
 
-    expect("The answer is 42." == s);
+    auto A = Mat{{1, 2}, {3, 4}};
+    auto B = Mat{{5, 6}, {7, 8}};
+    auto C = Mat{};
+
+    stx::linalg::matrix_product(A.span(), B.span(), C.span());
+
+    const auto s = fmt::format("The answer is {}.", C[0, 0]);
+
+    expect("The answer is 19." == s);
   };
+  // NOLINTEND(readability-magic-numbers)
 }
